73c600cf98f9a0647f4c8f7d9f93cec427ccb4e7
Rick South <rick@natamus.com>
2024-04-28 19:01:57 +0200

    (v2.6) (1.20.5 Neo) Updated to work with NeoForge's TickEvent changes in 20.5.14.

--- NeoForge/src/main/java/com/natamus/firespreadtweaks/neoforge/events/NeoForgeFireSpreadEvent.java
+++ NeoForge/src/main/java/com/natamus/firespreadtweaks/neoforge/events/NeoForgeFireSpreadEvent.java
@@ -4,8 +4,7 @@ import com.natamus.collective.functions.WorldFunctions;
 import com.natamus.firespreadtweaks.events.FireSpreadEvent;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
-import net.neoforged.neoforge.event.TickEvent.LevelTickEvent;
-import net.neoforged.neoforge.event.TickEvent.Phase;
+import net.neoforged.neoforge.event.tick.LevelTickEvent;
 import net.neoforged.neoforge.event.level.BlockEvent;
 import net.neoforged.neoforge.event.level.LevelEvent;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -14,9 +13,9 @@ import net.neoforged.fml.common.EventBusSubscriber;
 @EventBusSubscriber
 public class NeoForgeFireSpreadEvent {
 	@SubscribeEvent
-	public static void onWorldTick(LevelTickEvent e) {
-		Level level = e.level;
-		if (level.isClientSide || !e.phase.equals(Phase.END)) {
+	public static void onWorldTick(LevelTickEvent.Post e) {
+		Level level = e.getLevel();
+		if (level.isClientSide) {
 			return;
 		}
 
