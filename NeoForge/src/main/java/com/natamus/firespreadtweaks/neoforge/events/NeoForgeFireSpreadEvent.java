463d460c889f3c121d9f532be29b95584a531f99
Rick South <rick@natamus.com>
2024-04-28 16:03:21 +0200

    Fire Spread Tweaks source code for 1.20.5.

---
+++ NeoForge/src/main/java/com/natamus/firespreadtweaks/neoforge/events/NeoForgeFireSpreadEvent.java
@@ -0,0 +1,55 @@
+package com.natamus.firespreadtweaks.neoforge.events;
+
+import com.natamus.collective.functions.WorldFunctions;
+import com.natamus.firespreadtweaks.events.FireSpreadEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.event.TickEvent.LevelTickEvent;
+import net.neoforged.neoforge.event.TickEvent.Phase;
+import net.neoforged.neoforge.event.level.BlockEvent;
+import net.neoforged.neoforge.event.level.LevelEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeFireSpreadEvent {
+	@SubscribeEvent
+	public static void onWorldTick(LevelTickEvent e) {
+		Level level = e.level;
+		if (level.isClientSide || !e.phase.equals(Phase.END)) {
+			return;
+		}
+
+		FireSpreadEvent.onWorldTick((ServerLevel)level);
+	}
+	
+	@SubscribeEvent
+	public static void onWorldLoad(LevelEvent.Load e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		FireSpreadEvent.onWorldLoad((ServerLevel)level);
+	}
+	
+	@SubscribeEvent
+	public static void onWorldUnload(LevelEvent.Unload e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}		
+
+		FireSpreadEvent.onWorldUnload((ServerLevel)level);
+	}
+	
+	@SubscribeEvent
+	public static void onNeighbourNotice(BlockEvent.NeighborNotifyEvent e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		FireSpreadEvent.onNeighbourNotice(level, e.getPos(), e.getState(), e.getNotifiedSides(), e.getForceRedstoneUpdate());
+	}
+}
