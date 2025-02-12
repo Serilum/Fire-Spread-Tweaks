42fc065f81ab39b4a5914dbca3012a76c0e9c333
Rick South <rick@natamus.com>
2025-02-12 23:34:08 +0100

    (v2.8) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/firespreadtweaks/ModForge.java
+++ Forge/src/main/java/com/natamus/firespreadtweaks/ModForge.java
@@ -32,7 +32,7 @@ public class ModForge {
 	}
 
 	private void loadComplete(final FMLLoadCompleteEvent event) {
-    	MinecraftForge.EVENT_BUS.register(new ForgeFireSpreadEvent());
+    	MinecraftForge.EVENT_BUS.register(ForgeFireSpreadEvent.class);
 	}
 
 	private static void setGlobalConstants() {
--- NeoForge/src/main/java/com/natamus/firespreadtweaks/neoforge/events/NeoForgeFireSpreadEvent.java
+++ NeoForge/src/main/java/com/natamus/firespreadtweaks/neoforge/events/NeoForgeFireSpreadEvent.java
@@ -8,9 +8,7 @@ import net.neoforged.neoforge.event.tick.LevelTickEvent;
 import net.neoforged.neoforge.event.level.BlockEvent;
 import net.neoforged.neoforge.event.level.LevelEvent;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 
-@EventBusSubscriber
 public class NeoForgeFireSpreadEvent {
 	@SubscribeEvent
 	public static void onWorldTick(LevelTickEvent.Post e) {
