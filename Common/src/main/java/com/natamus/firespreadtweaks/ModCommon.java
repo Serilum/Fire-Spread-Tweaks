11735c0a748bded9c5c150ae55b3ac56bf75c948
Rick South <rick@natamus.com>
2025-07-18 03:02:40 +0200

    Fire Spread Tweaks source code for 1.21.8.

---
+++ Common/src/main/java/com/natamus/firespreadtweaks/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.firespreadtweaks;
+
+import com.natamus.firespreadtweaks.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
