887cd6081c5661bc638adeca9767f8395bae5335
Rick South <rick@natamus.com>
2026-06-18 23:54:49 +0200

    Updated Fire Spread Tweaks to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/firespreadtweaks/util/Reference.java
+++ Common/src/main/java/com/natamus/firespreadtweaks/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "firespreadtweaks";
 	public static final String NAME = "Fire Spread Tweaks";
 	public static final String VERSION = "2.8";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
